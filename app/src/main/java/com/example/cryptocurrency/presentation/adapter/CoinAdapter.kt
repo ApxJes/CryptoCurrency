9587b41fa9e0f2fdea07a8994ad1bbc9382cbf8c
aungpyaekyaw <shwyzee109@gmail.com>
2025-03-05 18:34:45 +0630

    Appear crypto currency

---
+++ app/src/main/java/com/example/cryptocurrency/presentation/adapter/CoinAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.cryptocurrency.presentation.adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptocurrency.R
+import com.example.cryptocurrency.domain.model.Coin
+
+class CoinAdapter(
+    private val context: Context
+) : RecyclerView.Adapter<CoinAdapter.ItemViewHolder>() {
+
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val coinName: TextView = itemView.findViewById(R.id.txvCoinName)
+        val coinSymbol: TextView = itemView.findViewById(R.id.txvCoinSymbol)
+        val isActiveCoin: TextView = itemView.findViewById(R.id.txvIsActive)
+    }
+
+    private val differCallBack = object : DiffUtil.ItemCallback<Coin>() {
+        override fun areItemsTheSame(oldItem: Coin, newItem: Coin): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Coin, newItem: Coin): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallBack)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.coin_item, parent, false)
+        return ItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val coin = differ.currentList[position]
+
+        holder.apply {
+            coinName.text = coin.name
+            coinSymbol.text = "(${coin.symbol})"
+            isActiveCoin.text = if (coin.isActive) "Active" else "Inactive"
+
+            if (coin.isActive) {
+                isActiveCoin.setTextColor(ContextCompat.getColor(context, R.color.green ))
+            } else {
+                isActiveCoin.setTextColor(ContextCompat.getColor(context, R.color.red))
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cryptocurrency/presentation/ui/CoinsListFragment.kt
@@ -0,0 +1,84 @@
+package com.example.cryptocurrency.presentation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cryptocurrency.R
+import com.example.cryptocurrency.databinding.FragmentCoinsListBinding
+import com.example.cryptocurrency.presentation.adapter.CoinAdapter
+import com.example.cryptocurrency.presentation.coins_list.CoinsListViewModel
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class CoinsListFragment : Fragment() {
+
+    private var _binding: FragmentCoinsListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var coinAdapter: CoinAdapter
+    private val coinsListViewModel: CoinsListViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCoinsListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        coinAdapter = CoinAdapter(requireContext())
+
+        collectState()
+        recyclerViewCoinsList()
+
+    }
+
+    private fun collectState() {
+        lifecycleScope.launchWhenStarted {
+            coinsListViewModel.state.collect { state ->
+                if(state.isLoading) {
+                    showProgressBar()
+                } else {
+                    hideProgressBar()
+                }
+
+                state.coins.let {
+                    coinAdapter.differ.submitList(it)
+                }
+
+                if(state.error.isNotEmpty()) {
+                    Snackbar.make(binding.root, state.error, Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun recyclerViewCoinsList() {
+        binding.rcvCoinsList.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = coinAdapter
+        }
+    }
+
+    private fun showProgressBar() {
+        binding.prgBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        binding.prgBar.visibility = View.GONE
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
